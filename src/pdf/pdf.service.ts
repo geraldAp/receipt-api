c9167007cdf541eca3d027387fb7c6e3e1952f64
geraldAp <amankwahprincegerald19@gmail.com>
2025-03-14 18:26:42 +0000

    feat: Added pdf generation service

--- src/pdf/pdf.service.ts
+++ src/pdf/pdf.service.ts
@@ -1,77 +1,18 @@
+import * as puppeteer from 'puppeteer';
 import { Injectable } from '@nestjs/common';
-import { PDFDocument, rgb } from 'pdf-lib';
 
 @Injectable()
 export class PdfService {
-  async generatePdf(
-    receiptData: {
-      // Optional title for branding
-      heading: string[];
-      data: { name: string; value: string | number }[];
-      footer: { name: string; value: string | number }[];
-    },
-    title: string,
-  ) {
-    const pdfDoc = await PDFDocument.create();
-    const page = pdfDoc.addPage([600, 400]); // Width: 600, Height: 400
-
-    let y = 380; // Start from the top of the page
-    const { heading, data, footer } = receiptData;
-
-    // Add Title (Optional Branding)
-    if (title) {
-      page.drawText(title, {
-        x: 50,
-        y,
-        size: 18,
-        color: rgb(0, 0.53, 0.71), // Blue-ish color
-      });
-      y -= 30; // Add spacing after the title
+  async generatePdf(emailTemaplate: string) {
+    try {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      await page.setContent(emailTemaplate);
+      const pdfBuffer = await page.pdf({ format: 'A4' });
+      await browser.close();
+      return pdfBuffer;
+    } catch (error) {
+      console.error(error);
     }
-
-    // Add Heading
-    page.drawText(`${heading[0]}: ${heading[1]}`, {
-      x: 50,
-      y,
-      size: 14,
-      color: rgb(0, 0, 0), // Black
-    });
-    y -= 20; // Spacing after the heading
-
-    // Add Data Rows
-    data.forEach((item) => {
-      // Draw item name on the left
-      page.drawText(`${item.name}`, {
-        x: 50,
-        y,
-        size: 12,
-        color: rgb(0, 0, 0),
-      });
-
-      // Draw item value on the right
-      page.drawText(`${item.value}`, {
-        x: 500, // Align value to the right
-        y,
-        size: 12,
-        color: rgb(0, 0, 0),
-      });
-
-      y -= 20; // Move to the next row
-    });
-
-    // Add Footer
-    y -= 20; // Add extra spacing before footer
-    footer.forEach((item) => {
-      page.drawText(`${item.name}: ${item.value}`, {
-        x: 50,
-        y,
-        size: 12,
-        color: rgb(0.53, 0, 0), // Dark red for footer
-      });
-      y -= 20;
-    });
-
-    // Save the PDF and return its bytes
-    return await pdfDoc.save();
   }
 }
